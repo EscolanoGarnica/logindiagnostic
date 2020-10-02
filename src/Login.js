9c187bdea81432d7466cc05705d4aedf57787227
Laura <laura@Dinbeat-MacbookPRO-software.local>
2020-10-02 10:18:33 +0200

    inicio

---
+++ src/Login.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import dog from "./img/dog_nocopyright.jpg";
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <img class="imageDog" src={dog} alt="dog photo"></img>
+
+        <div class="login">
+
+          <div class="loginContainer">
+
+            <div class="inputsContainer">
+              <p class="letras"> Correo electrónico</p>
+              <input
+                class="input"
+                type="text"
+                onChange={this.myChangeHandler}
+              />
+            </div>
+
+            <div class="inputsContainer">
+              <p class="letras">Contraseña</p>
+              <input
+                class="input"
+                type="text"
+                onChange={this.myChangeHandler}
+              />
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
